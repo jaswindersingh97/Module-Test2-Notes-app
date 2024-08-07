9356550bf7f1843c9259a3834d474c63a9d1657f
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-08-07 18:41:51 +0530

    changes done on add button

--- NotesApp/src/App.jsx
+++ NotesApp/src/App.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css'
 import Left from './components/Left';
-// import Right from './components/Right';
+import Right from './components/Right';
 import RightActual from './components/RightActual';
+import AddNote from './components/AddNote';
 function App() {
   return (
+    <>
     <div className="container">
-      <>
-        <Left/>
-      </>
+        {/* <Left/> */}
       <div className='right'>
       {/* <Right/> */}
-      <RightActual param={"groupname"}/>
+      {/* <RightActual param={"groupname"}/> */}
       </div>
     </div>
+    <div className='BlackScreen'> {/* Rename when needed */}
+    <div className='AddNote'>
+      <AddNote/>
+    </div>
+    </div>
+    </>
   )
 }
 
