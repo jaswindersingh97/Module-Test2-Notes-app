fd4a98f257050b9a89a46fd6f24cbcc6dabce357
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-08-08 14:13:53 +0530

    commit

--- NotesApp/src/utils/DateTime.jsx
+++ NotesApp/src/utils/DateTime.jsx
@@ -1,10 +1,9 @@
 
 function DateTime() {
     const day = new Date();
-    const m = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"];
-    const str_op = day.getDate() + ' ' + m[day.getMonth()] + ' ' + day.getFullYear();
-    return(str_op);    
+    const m = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    const date = day.getDate() + ' ' + m[day.getMonth()] + ' ' + day.getFullYear();
+    return(date);
 }
 
 export default DateTime
